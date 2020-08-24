5ae8a37d91faff5d755de130b0de5e13bc217c48
matthias haefeli <haefeli.matthias@gmail.com>
2020-08-24 14:58:49 -0600

    refactor to class component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>albumlist</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>albumlist</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
