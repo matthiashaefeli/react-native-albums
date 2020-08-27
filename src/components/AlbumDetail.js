35d84cb5b18c689f7ee62c1d73086b7b2d4dc97b
matthias haefeli <haefeli.matthias@gmail.com>
2020-08-27 12:55:14 -0600

    add link to url and change button text as children

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import AlbumButton from './AlbumButton'
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const {
     thumbnailStyle,
     headerContenStyle,
@@ -35,7 +35,9 @@ const AlbumDetail = ({ album }) => {
         />
       </CardSection>
       <CardSection>
-        <AlbumButton />
+        <AlbumButton onPress={() => Linking.openURL(url)}>
+          Buy Now!
+        </AlbumButton>
       </CardSection>
     </Card>
   )
